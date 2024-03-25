291347724d46b6663680cdc341d80b4023fc5c74
Jonas Wallmann <jonas.wallmann@t-online.de>
2024-03-25 12:16:40 +0000

    Fix video stream alignment

--- AVJ Math/GameView.swift
+++ AVJ Math/GameView.swift
@@ -22,6 +22,7 @@ struct GameView: View {
                         fingerVM: fingerVM,
                         cardVM: cardVM
                     )
+                    .frame(maxHeight: .infinity)
 
                     ForEach(fingerVM.points, id: \.id) { finger in
                         if finger.isCounted {
